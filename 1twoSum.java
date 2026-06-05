550266f5f775d9778c9444c90754be2d1abcf414
uthkarsh15 <shivamg1505@gmail.com>
2026-06-06 01:38:35 +0530

    bruteTwoSum

--- 1twoSum.java
+++ 1twoSum.java
@@ -1,5 +1,20 @@
+// bruute force solution
+class Solution1 {
+    public int[] twoSum(int[] nums, int target) {
+        int n = nums.length;
+        for(int i=0; i<n-1; i++){
+            for(int j=i+1; j<n; j++){
+                if(target == nums[i] + nums[j]){
+                    return new int[] {i,j};
+                }
+            }
+        }
+        return new int[] {-1,-1};
+    }
+}
+
 // optimal solution using hash map
-class Solution {
+class Solution2 {
     public int[] twoSum(int[] nums, int target) {
         int n = nums.length;
         HashMap<Integer, Integer> map = new HashMap<>();
