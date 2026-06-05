cb2e8a26be2eb4697006101a74dfb2d354b091c9
uthkarsh15 <shivamg1505@gmail.com>
2026-06-05 23:58:04 +0530

    bruteTwoSumII

--- twoSumII.java
+++ twoSumII.java
@@ -1,3 +1,19 @@
+// brute force solution
+class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+        int n = numbers.length;
+        for(int i=0; i<n-1; i++){
+            for(int j=i+1; j<n; j++){
+                if(target == numbers[i]+numbers[j]){
+                    return new int[] {i+1,j+1};
+                }
+            }
+        }
+        return new int[] {-1,-1};
+    }
+}
+
+
 // optimal solution using two pointer approach
 class Solution {
     public int[] twoSum(int[] numbers, int target) {
