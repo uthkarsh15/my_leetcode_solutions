1d479bee1214acb14c3460e48e3a8c24f8f7689a
uthkarsh15 <shivamg1505@gmail.com>
2026-06-06 00:15:20 +0530

    subOptimalTwoSumII

--- twoSumII.java
+++ twoSumII.java
@@ -13,6 +13,22 @@ class Solution {
     }
 }
 
+// sub-optimal solution using hash map
+class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+        int n = numbers.length;
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for(int i=0; i<n; i++){
+            int complement = target - numbers[i];
+            if(map.containsKey(complement)){
+                return new int[] {(map.get(complement))+1, i+1};
+            }
+            map.put(numbers[i], i);
+        }
+        return new int[] {-1,-1};
+    }
+}
 
 // optimal solution using two pointer approach
 class Solution {
