dd01a8e3fa346d0c35bba79b9068e25153097512
uthkarsh15 <shivamg1505@gmail.com>
2026-06-25 23:51:18 +0530

    2520CounttheDigitsThatDivideaNumber

---
+++ 2520CounttheDigitsThatDivideaNumber.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int countDigits(int num) {
+        int original = num; 
+        int count=0;
+        while(num > 0){
+            int digit = num % 10;
+            num = num / 10; 
+            if(original % digit==0){
+                count++;
+            }
+        }
+        return count;
+    }
+}
