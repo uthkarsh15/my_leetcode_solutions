d55c6a1d3b750bedf136d01f80c76124dc723838
uthkarsh15 <shivamg1505@gmail.com>
2026-06-27 20:21:43 +0530

    974SubarraySumsDivisiblebyK

---
+++ 974SubarraySumsDivisiblebyK.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int subarraysDivByK(int[] nums, int k) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(0,1);
+        int currSum = 0;
+        int count = 0;
+        for(int num : nums){
+            currSum +=num;
+            
+            int rem = currSum % k;
+            if(rem < 0){
+                rem += k;
+            }
+
+            if(map.containsKey(rem)){
+                count += map.get(rem);
+            }
+
+            map.put(rem, map.getOrDefault(rem, 0)+1);
+        }
+        return count;
+    }
+}
