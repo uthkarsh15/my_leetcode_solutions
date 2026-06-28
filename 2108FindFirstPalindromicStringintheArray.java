a1f9c32fb60122d0c703d218c3223a2e4f22d399
uthkarsh15 <shivamg1505@gmail.com>
2026-06-28 23:44:38 +0530

    2108FindFirstPalindromicStringintheArray

---
+++ 2108FindFirstPalindromicStringintheArray.java
@@ -0,0 +1,23 @@
+class Solution {
+    public String firstPalindrome(String[] words) {
+        for(String word : words){
+            int left = 0;
+            int right = word.length() -1;
+            boolean isPalindrome = true;
+
+            while(left < right){
+                if(word.charAt(left) != word.charAt(right)){
+                    isPalindrome = false;
+                    break;
+                }
+                left++;
+                right--;
+            }
+            if(isPalindrome){
+                return word;
+            }
+        }
+        return "";
+        
+    }
+}
