f9d12ae58ef830a944296aa0e132a01ab4104a2b
uthkarsh15 <shivamg1505@gmail.com>
2026-07-10 23:38:48 +0530

    709ToLowerCase

---
+++ 709ToLowerCase.java
@@ -0,0 +1,11 @@
+class Solution {
+    public String toLowerCase(String s) {
+        char arr[] = s.toCharArray();
+        for(int i=0; i<arr.length; i++){
+            if(arr[i] >= 'A' && arr[i] <= 'Z'){
+                arr[i] = (char) (arr[i] + 32);
+            }
+        }
+        return new String(arr);
+    }
+}
