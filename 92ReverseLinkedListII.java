cd27d3be8c02c30b337ebd5671019bf4227fcf72
uthkarsh15 <shivamg1505@gmail.com>
2026-07-30 00:02:24 +0530

    92fix

--- 92ReverseLinkedListII.java
+++ 92ReverseLinkedListII.java
@@ -1,3 +1,49 @@
-public class 92ReverseLinkedListII {
-    
+
+//  Definition for singly-linked list.
+class ListNode {
+     int val;
+     ListNode next;
+     ListNode() {}
+     ListNode(int val) { this.val = val; }
+     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
+
+class Solution {
+    public ListNode reverseBetween(ListNode head, int left, int right) {
+        if(head==null){
+            return null;
+        }
+        if(left==right){
+            return head;
+        }
+        ListNode t = head;
+        ListNode before = null;
+        int pos =1;
+        while(t!=null){
+            if(pos<left){
+                before = t;
+                t = t.next;
+                pos++;
+                continue;
+            }
+            //pos = left
+            ListNode curr = t;
+            ListNode prev = null;
+            int times = right - left +1;
+            while(times>0){
+                ListNode nex = curr.next;
+                curr.next = prev;
+                prev = curr;
+                curr = nex;
+                times--;
+            }
+            t.next = curr;
+            if(before!=null){
+                before.next = prev;
+                return head;
+            }
+            return prev;
+        }
+        return head;
+    }
 }
